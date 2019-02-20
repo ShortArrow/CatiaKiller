cd98bd96f3d229a8cbd6071ff79844458b77af35
ShortArrow <bamboogeneral@live.jp>
2019-02-20 17:24:42 +0900

    add main

---
+++ main.ps1
@@ -0,0 +1,12 @@
+$code = @'
+    [DllImport("user32.dll")]
+     public static extern IntPtr GetForegroundWindow();
+'@
+Add-Type $code -Name Utils -Namespace Win32
+while (1) {
+    $hwnd = [Win32.Utils]::GetForegroundWindow()
+    Write-Host(Get-Process | 
+            Where-Object { $_.mainWindowHandle -eq $hwnd } | 
+            Select-Object processName, MainWindowTItle, MainWindowHandle).Tostring()
+    sleep -Milliseconds 200
+}
