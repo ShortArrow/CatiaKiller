0f63f50b6ecb17faff0d39e287d395012dab15dd
ShortArrow <bamboogeneral@live.jp>
2019-02-21 15:23:28 +0900

    add WindowStyle

--- launch.cmd
+++ launch.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo "ps1ファイルを実行します"
 pushd %~dp0
-powershell -NoProfile -ExecutionPolicy Unrestricted "./main.ps1"
+powershell -NoProfile -WindowStyle Hidden -ExecutionPolicy Unrestricted "./main.ps1"
 pause > nul
 exit
